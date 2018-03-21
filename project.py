85521fa1d21799f08b7b5e5586dc9f58002063df
SubrataBauri <subrata@digitalaptech.com>
2018-03-22 02:45:03 +0530

    Adding markers to the map

--- project.py
+++ project.py
@@ -1,12 +1,25 @@
 import folium
 
 # create map object
-map = folium.Map(location=[22.622033, 88.450197], zoom_start=13)
+map = folium.Map(location=[22.622033, 88.450197], zoom_start=14)
+
+# create a terrain map
+map2 = folium.Map(location=[22.622033, 88.450197], zoom_start=13, tiles="Stamen Terrain")
+
+# map.simple_marker(location=[22.622033, 88.450197], popup="City Center 2", marker_color="red")
+# map.simple_marker(location=[22.621086, 88.458555], popup="NKDA Stadium", marker_color="green")
+
+folium.Marker(location=[22.622033, 88.450197],
+              popup="City Center 2, \n New Town",
+              icon=folium.Icon(color='red', icon='info-sign')
+              ).add_to(map)
+folium.Marker(location=[22.621086, 88.458555],
+              popup="NKDA Stadium",
+              icon=folium.Icon(color='green', icon='info-sign')
+              ).add_to(map)
 
 # map.create_map(path="index.html") # doesn't have it anymore
 
 map.save("index.html") # create map file
 
-# create a terrain map
-map2 = folium.Map(location=[22.622033, 88.450197], zoom_start=13, tiles="Stamen Terrain")
 map2.save("terrain-map.html")
