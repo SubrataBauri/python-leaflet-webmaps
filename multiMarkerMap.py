bab608a3b7b1b5e7368a42ee73d8a7ed4029c4e7
SubrataBauri <subrata@digitalaptech.com>
2018-03-22 17:49:01 +0530

    move inline conditionals to method

--- multiMarkerMap.py
+++ multiMarkerMap.py
@@ -6,11 +6,22 @@ dataFrame = pandas.read_csv("data_CSV.csv")
 # create a terrain map
 map = folium.Map(location=[45.372, -121.697], zoom_start=5, tiles="Stamen Terrain")
 
-for lat,lon,name,elev in zip(dataFrame["LAT"], dataFrame["LON"], dataFrame["NAME"], dataFrame["ELEV"]):
+# create method for conditional colouring
+def color(elev):
+    if elev in range(1, 1000):
+        col = "green"
+    elif elev in range(1000, 3000):
+        col = "blue"
+    else:
+        col="red"
+    return col
 
-    folium.Marker(location = [lat, lon],
-                  popup = name,
-                  icon = folium.Icon(color='green' if elev in range(0, 1000) else 'orange' if elev in range(1000, 3000) else 'red', icon='info-sign')
+
+for lat, lon, name, elev in zip(dataFrame["LAT"], dataFrame["LON"], dataFrame["NAME"], dataFrame["ELEV"]):
+
+    folium.Marker(location=[lat, lon],
+                  popup=name,
+                  icon=folium.Icon(color=color(elev), icon='info-sign')
                   ).add_to(map)
 
 map.save("multi-marker-map.html") # create map file
