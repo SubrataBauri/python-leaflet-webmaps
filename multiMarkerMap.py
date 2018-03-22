769bdd0c9a6328b4b4d1e31f0dce49a370cea65c
SubrataBauri <subrata@digitalaptech.com>
2018-03-22 19:49:24 +0530

    give-map-center-dynamic

--- multiMarkerMap.py
+++ multiMarkerMap.py
@@ -4,13 +4,16 @@ import pandas
 dataFrame = pandas.read_csv("data_CSV.csv")
 
 # create a terrain map
-map = folium.Map(location=[45.372, -121.697], zoom_start=5, tiles="Stamen Terrain")
+#map = folium.Map(location=[45.372, -121.697], zoom_start=5, tiles="Stamen Terrain")
+map = folium.Map(location=[dataFrame["LAT"].mean(), dataFrame["LON"].mean()], zoom_start=5, tiles="Stamen Terrain")
 
 # create method for conditional colouring
 def color(elev):
-    if elev in range(1, 1000):
+    minimum=int(min(dataFrame["ELEV"]))
+    step=int((max(dataFrame["ELEV"]) - min(dataFrame["ELEV"]))/3)
+    if elev in range(minimum, minimum+step):
         col = "green"
-    elif elev in range(1000, 3000):
+    elif elev in range(minimum+step, minimum+step*2):
         col = "blue"
     else:
         col="red"
